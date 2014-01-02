30c201381d3ed9c2788b492fb7a0750d4f8527b4
Peter Sieg <chasingmaxwell@gmail.com>
2014-01-02 17:47:34 -0500

    Add support for Drupal 7.

--- boris-loader.php
+++ boris-loader.php
@@ -4,5 +4,6 @@ require_once __DIR__.'/src/Boris/Loader/Provider/ProviderInterface.php';
 require_once __DIR__.'/src/Boris/Loader/Provider/AbstractProvider.php';
 require_once __DIR__.'/src/Boris/Loader/Provider/Composer.php';
 require_once __DIR__.'/src/Boris/Loader/Provider/Drupal.php';
+require_once __DIR__.'/src/Boris/Loader/Provider/Drupal7.php';
 require_once __DIR__.'/src/Boris/Loader/Provider/Symfony.php';
 require_once __DIR__.'/src/Boris/Loader/Provider/EzPublish.php';
---
+++ src/Boris/Loader/Provider/Drupal7.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Boris\Loader\Provider;
+
+class Drupal7 extends AbstractProvider
+{
+    public $name = 'drupal7';
+
+    public function assertDir($dir)
+    {
+      return (file_exists("$dir/includes/common.inc") && file_exists("$dir/misc/drupal.js") && ((basename($dir) != 'core') || !file_exists("$dir/../composer.json")));
+    }
+
+    public function initialize(\Boris\Boris $boris, $dir)
+    {
+        parent::initialize($boris, $dir);
+
+        chdir($dir);
+
+        define('DRUPAL_ROOT', $dir);
+
+        require_once "$dir/includes/bootstrap.inc";
+
+        drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+    }
+}
+
