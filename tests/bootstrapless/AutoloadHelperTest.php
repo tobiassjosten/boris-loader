e22323ae6bd6b4342ce837510e4e6c560be30902
Tobias Sjösten <tobias@tobiassjosten.net>
2014-01-29 17:41:43 +0100

    Added bootstrapless test suite

---
+++ tests/bootstrapless/AutoloadHelperTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Boris\Loader\Test;
+
+class AutoloadHelperTest extends \PHPUnit_Framework_TestCase
+{
+    public function testIncludeHelper()
+    {
+        include __DIR__.'/../../boris-loader.php';
+    }
+}
